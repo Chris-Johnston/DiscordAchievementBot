7ea96c91f50942e05d0298d6cee7e0ac477152c8
Chris Johnston <githubchrisjohnston@gmail.com>
2017-08-16 21:11:57 -0700

    Added unit tests. Also added image deletion after message is sent.

--- DiscordAchievementBot/Configuration/Configuration.cs
+++ DiscordAchievementBot/Configuration/Configuration.cs
@@ -18,5 +18,12 @@ namespace DiscordAchievementBot
         // image stuff
         [XmlElementAttribute("ImageTemporaryDirectory")]
         public string ImageTemporaryDirectory;
+
+        // paths to the background images for each of the achievement presets
+        // in the Resources folder which should be configured to always copy to output directory
+        public const string Path_AchievementXboxOneBackground = @"Resources\xboxone.png";
+        public const string Path_AchievementXboxOneRareBackground = @"Resources\xboxone.png";
+        public const string Path_AchievementXbox360Background = @"Resources\test.png";
+
     }
 }
